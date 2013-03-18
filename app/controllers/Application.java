d6e45c00757f88312654153243b198182447432d
Arpit Gautam <arpit.gautam@outlook.com>
2013-03-19 01:56:49 +0530

    style added

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,17 @@
 package controllers;
 
+import models.Project;
+import models.Task;
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render(
+        		Project.finder.all(),
+        		Task.find.all()));
     }
   
 }
